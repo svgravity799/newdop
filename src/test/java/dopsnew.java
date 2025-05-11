dcd8ee8de2ea937dd92b9623048d4c0c3ee5beb0
pavelyatmanov <svgravity799@gmail.com>
2025-05-11 11:13:26 -0700

    firstCommit2

--- src/test/java/dopsnew.java
+++ src/test/java/dopsnew.java
@@ -1,4 +1,4 @@
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.*;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import static com.codeborne.selenide.Selenide.*;
 
 import java.time.Duration;
 
@@ -52,6 +53,7 @@ public class dopsnew {
 
         WebElement countrySearch = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class=v-departure__pinput]")));
         countrySearch.click();
+        $(byText("Россия")).click();
 
 
 
@@ -66,7 +68,94 @@ public class dopsnew {
         //  $(byText("Выбрать")).click();
     }
 
+    @Test
+    void ostrovokTest() {
+        Configuration.browser = "chrome"; // <--- важно
+        Configuration.pageLoadStrategy = "none"; // или "none", если хочешь ускорить
+
+        Selenide.open("https://ostrovok.ru/");
+
+        $("[class=Control-module__value--1KWj3]").click();
+    }
 
 
+    @Test
+    void fsLogin2()
+    {
+
+        Configuration.browser = "chrome"; // <--- важно
+        Configuration.pageLoadStrategy = "none"; // или "none", если хочешь ускорить
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "medium";
+
+        Selenide.open("https://fstravel.com/searchexcursions");
+        $("[class=v-icon-user-14]").click(); // Открываем попап Лк
+        $("[type=email]").setValue("testfspavelclient@yandex.ru"); // Вводим логин
+        $("[type=password]").setValue("Abonent13!");$("[type=submit]").click();  // Вводим пароль
+
+System.out.println("Авторизация выполнена");
+
+
+        $("div[class=v-departure__pinput]").click(); // Открываем попап Страна поездки
+        sleep(1500);
+        $(byText("Турция")).click(); // Выбираем Турцию
+        $("div[class=calendar__field-dates]").click(); // Открываем Календарь
+        $("[class=calendar-popup__arrow-right]").click();  //Промотка календаря
+        $("[class=calendar-popup__arrow-right]").click();
+        $("[class=calendar-popup__arrow-right]").click();
+        $("[class=calendar-popup__arrow-right]").click();
+        $("[class=calendar-popup__arrow-right]").click();
+        $("[class=calendar-popup__arrow-right]").click();
+        $(byText("12")).click();  // Выбираем дату в Календаре 12
+        $(byText("13")).click();  // Выбираем дату в Календаре 13
+        $("span[class=tourists__title]").click(); // Открывает Попап с количеством туристов
+   $("span.v-icon-minus_square").shouldBe(Condition.visible).click(); // Уменьшаем количество туристов в Попапе до одного
+        $("button.v-search-button").click(); // Нажимаем поиск
+
+        System.out.println("Поиск выполнен");
+
+        sleep(2000);
+
+
+
+
+        // Ждём, пока появятся карточки
+        ElementsCollection cards_of_vidacha = $$("div.excursion-card__content").shouldHave(CollectionCondition.sizeGreaterThan(1));  // Ждём, пока появятся карточки
+        cards_of_vidacha.get(1).$("button").click();  // Кликаем на кнопку внутри второй карточки
+
+
+        //  $("(//div[contains(@class,'excursion-card__content')])[1] //button").shouldBe(Condition.visible).click();
+//$("(//div[contains(@class,'excursion-card__content')])[2] //button)])").click
+
+
+
+
+    //    $("[class=v-search-button]").click();
+      //  System.out.println("Поиск выполнен");
+
+
+       // WebElement iconMinus = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class=v-icon-minus_square]")));
+       // iconMinus.click();
+
+
+       // $(byText("12")).click();
+       // $(byText("2")).click();  //выбор количества ночей
+       // $(byText("2")).click();
+       // $("[data-testid=tours_searchblock_field_tourists_popup_Adminus]").click();  //уменьшение количества туристов
+       // $("[data-testid=tours_searchblock_search_btn]").click();  //выполнение поиска
+
+
+
+
+//$("div[class=v-departure__pinput]").click();
+//$(byText("Россия")).click();
+//sleep(3000);
+
+
+    }
 
 }
+
+
+
+
